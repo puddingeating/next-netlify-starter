2be4f040256524bc3733c5bd49cb3f347985c42b
puddingeating <kofururu@gmail.com>
2024-06-02 02:17:57 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>河田家のプリンさん</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
